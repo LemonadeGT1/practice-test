25d9cd3b52fde8448efd584a90790a09b3142bf6
LemonadeGT1 <Michael.P.Patience@gmail.com>
2023-01-30 17:08:00 -0700

    First Update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 18
+const ageToVote = 100
 
 const user = {
   name: 'Sherlock Holmes',
@@ -17,5 +17,33 @@ const cases = [
 ]
 
 // YOUR JS CODE
+let holmesCase = {
+  title: "",
+  year: 0
+}
 
+let currentYear = 2023
+let jsReleaseYear = 1995
+console.log("current age of JavaScript is " + (currentYear - jsReleaseYear).toString());
 
+if (currentYear - jsReleaseYear < ageToVote) {
+  console.log("JavaScript is not old enough to vote")
+}
+else {
+  console.log("JavaScript is old enough to vote")
+}
+
+console.log("Starting")
+for(i=0; i<=100; i+= 10){
+  if (i==50){console.log("Halfway")}
+  console.log(i)
+}
+console.log("Finished")
+
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
+
+cases.forEach(holmesCase => {
+  if (holmesCase.releaseYear == 1903){
+  console.log(`${holmesCase.title}, ${holmesCase.releaseYear}`)
+  }
+})
